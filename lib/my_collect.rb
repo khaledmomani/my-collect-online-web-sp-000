9af6c04e21472e08676683201aab1f359692e33b
Khaled S Momani <khaledmomani4@gmail.com>
2021-01-25 00:03:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(name)
   i = 0 
   collection = []
   while i < name.length
-  collection << name.split(" ") {first.yield(name[i])
+  collection << name.split(" ") {first.yield(name[i])}
   i += 1 
 end 
   
